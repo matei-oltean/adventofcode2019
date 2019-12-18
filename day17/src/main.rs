9dbeb7303185f16034262975b78d7054cac285ac
matei-oltean <matei.oltean@gmail.com>
2019-12-18 16:32:11 +0100

    Reformat day 17

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -10,23 +10,15 @@ fn main() {
             print!("#");
         }
     }
-    let mut l = 0;
-    for (i, val) in scaffolds.iter_mut().enumerate() {
-        if val == &10 {
-            l = i;
-            break;
-        }
-    }
-    let map_1d: Vec<_> = scaffolds
-        .iter()
-        .filter(|x| **x as usize != 10)
-        .collect::<Vec<_>>();
-    let map: Vec<_> = map_1d.chunks(l).collect();
+    let map: Vec<_> = scaffolds
+        .split(|x| *x == 10)
+        .filter(|slice| !slice.is_empty())
+        .collect();
     let mut res = 0;
     for i in 1..map.len() - 1 {
-        for j in 1..l - 1 {
-            if (0..3).all(|k| map[i + k - 1][j] == &35)
-                && (0..2).all(|k| map[i][j + 2 * k - 1] == &35)
+        for j in 1..map[i].len() - 1 {
+            if (0..3).all(|k| map[i + k - 1][j] == 35)
+                && (0..2).all(|k| map[i][j + 2 * k - 1] == 35)
             {
                 res += i * j;
             }
